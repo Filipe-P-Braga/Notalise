3387ab5d70b799da5b7d0a7544c61dce270a3db1
Lucas Bertolino <102763228+lucasbertolinogb@users.noreply.github.com>
2026-03-05 19:31:10 -0300

    Create event.cs

---
+++ event.cs
@@ -0,0 +1,10 @@
+namespace Backend.Models
+{
+    public class Event
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string? Description { get; set; }
+        public float Score { get; set; }
+    }
+}
