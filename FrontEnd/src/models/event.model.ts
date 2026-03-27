a5b06df008b44900e66ada8b6e940387697a84e8
Lucas Bertolino <102763228+lucasbertolinogb@users.noreply.github.com>
2026-03-27 08:13:18 -0300

    Delete FrontEnd directory

--- FrontEnd/src/models/event.model.ts
+++
@@ -1,7 +0,0 @@
-export interface EventModel {
-  name: string;
-  description: string;
-  address: string;
-  startDate: Date;
-  endDate: Date;
-}
--- FrontEnd/src/models/stand.model.ts
+++
@@ -1,7 +0,0 @@
-export interface StandModel {
-  eventName: string;
-  startDate: string;
-  endDate: string;
-  description: string;
-  comments?: string[];
-}
