329ed84d8478cf0177d692508f676cc967c79a05
Filipe-P-Braga <filipedetonandodesde2003@gmail.com>
2026-05-14 18:41:11 -0300

    Configurações de Tunel

--- FrontEnd/src/server.ts
+++ FrontEnd/src/server.ts
@@ -10,7 +10,9 @@ import { join } from 'node:path';
 const browserDistFolder = join(import.meta.dirname, '../browser');
 
 const app = express();
-const angularApp = new AngularNodeAppEngine();
+const angularApp = new AngularNodeAppEngine({
+  allowedHosts: ['*'] // Permitir todos os hosts (qualquer domínio do tunnel ou acessos locais)
+});
 
 /**
  * Example Express Rest API endpoints can be defined here.
